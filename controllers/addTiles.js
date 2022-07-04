02bf358c0391bfb5252453756844ff4fa09551d1
AlanNJ <alannpn75@gmail.com>
2022-07-04 16:17:47 +0530

    added-tiles

--- controllers/addTiles.js
+++ controllers/addTiles.js
@@ -1,5 +1,5 @@
 const db = require("../models/index");
-const vinyl = db.Venyl;
+const Tiles = db.Tiles;
 const addTilesItem = async (req, res) => {
 	const img = req.files[0];
 	console.log(img.path);
@@ -7,7 +7,7 @@ const addTilesItem = async (req, res) => {
 
 	data = { img: img.path, title, subtitle };
 
-	const product = await vinyl.create(data);
+	const product = await Tiles.create(data);
 	res.json(product);
 };
 module.exports = { addTilesItem };
