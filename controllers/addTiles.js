3caba524752c4314de09dcacd3d0a3950c30047f
AlanNJ <alannpn75@gmail.com>
2022-07-04 16:09:59 +0530

    added-controllers

---
+++ controllers/addTiles.js
@@ -0,0 +1,13 @@
+const db = require("../models/index");
+const vinyl = db.Venyl;
+const addTilesItem = async (req, res) => {
+	const img = req.files[0];
+	console.log(img.path);
+	const { title, subtitle } = req.body;
+
+	data = { img: img.path, title, subtitle };
+
+	const product = await vinyl.create(data);
+	res.json(product);
+};
+module.exports = { addTilesItem };
